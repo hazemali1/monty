3c9a215571494baa9c5135f831fff10567e555c9
hazemali1 <hazemali1343@gmail.com>
2023-06-22 07:38:34 -0700

    rotl

--- rotl.c
+++ rotl.c
@@ -10,12 +10,15 @@ void _rotl(stack_t **h)
 	stack_t *q, *w;
 
 	q = *h;
-	*h = (*h)->next;
-	(*h)->prev = NULL;
-	q->next = NULL;
-	w = *h;
-	while (w->next != NULL)
-		w = w->next;
-	w->next = q;
-	q->prev = w;
+	if (q)
+	{
+		*h = (*h)->next;
+		(*h)->prev = NULL;
+		q->next = NULL;
+		w = *h;
+		while (w->next != NULL)
+			w = w->next;
+		w->next = q;
+		q->prev = w;
+	}
 }
